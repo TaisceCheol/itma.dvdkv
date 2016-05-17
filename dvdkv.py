124bc4afcff6736524b51cc0131289dd92cb7845
phoban01 <phoban01@gmail.com>
2016-05-17 13:08:28 +0100

    added dd iso creation

--- dvdkv.py
+++ dvdkv.py
@@ -1,4 +1,8 @@
-import click,subprocess,os
+###															###
+### INSPIRED BY https://github.com/hneto/DVD-Video-Archiver	###
+###															###
+
+import click,subprocess,os,json
 
 def get_dvd_mount_point():
 	cmd = ['diskutil','info','/dev/disk2']
@@ -8,37 +12,65 @@ def get_dvd_mount_point():
 	result,err = grep.communicate()
 	return result.split(':')[-1].strip()
 
-def extract_dvd_metadata(info):
-	cmd = ['mediainfo','--Output=XML','-f',info['mnt_point'],'2>&1']
+def extract_dvd_metadata():
+	global info
+	cmd = ['mediainfo','--Output=XML','-f',info['mnt_point']+'/VIDEO_TS','2>&1']
 	with open(info['basepath']+'_dvd.metadata.xml', "w") as file:
 		subprocess.call(cmd,stdout=file)
 
-def extract_iso_metadata(info)
+def extract_iso_metadata():
+	global info
 	cmd = ['mediainfo','--Output=XML','-f',info['iso_path'],'2>&1']
 	with open(info['basepath']+'_iso.metadata.xml', "w") as file:
 		subprocess.call(cmd,stdout=file)
 
-def create_iso(info):
+def create_structure():
+	global info
 	if not os.path.exists(info['writedir']):
 		os.mkdir(info['writedir'])
-	extract_dvd_metadata()
-	info['iso_path'] = info['basepath'] +'.iso'
-	cmd = ['dd',info['mnt_point'],info['iso_path']]
+	if not os.path.exists(info['basedir']):
+		os.mkdir(info['basedir'])
+
+def create_iso():
+	global info
+	# first unmount disk
+	cmd = ['diskutil','unmountDisk','/dev/disk2']
 	subprocess.call(cmd)
-	extract_iso_metadata()
+	info['iso_path'] = info['basepath'] +'.iso'
+	cmd = ['dd','if=/dev/disk2','of=%s'%info['iso_path']]
+	subprocess.call(cmd)
 
+def create_mkv():
+	global info
+	cmd = ['ffmpeg',
+	'-i',info['iso_path'],
+	'-vcodec','ffv1',
+	'-acodec','copy','-ac','2',
+	'-scodec','copy',
+	'-f',info['basepath'] + '.mkv'
+	]
 
-def inquisition():
+def inquisition(writedir):
+	global info
 	info = {}
-	info['title'] = click.prompt("Please enter DVD title")
-	info['date'] = click.prompt("Please enter date")
-	info['refno'] = click.prompt("Please enter REFNO")
+	# info['title'] = click.prompt(click.style("Please enter DVD title",fg='green'))
+	# info['date'] = click.prompt(click.style("Please enter date",fg='green'))
+	info['refno'] = click.prompt(click.style("Please enter REFNO",fg='green'))
 	info['objid'] = info['refno'].replace('-','').lower()
-	info['write_dir'] = click.prompt("Specify output location")
-	info['basepath'] = os.path.join(info['writedir'],info['objid'])
-	info['technician'] = click.prompt("Please enter your own name",default="Piaras Hoban")
+	info['writedir'] = writedir
+	info['basedir'] = os.path.join(info['writedir'],info['objid'])
+	info['basepath'] = os.path.join(info['writedir'],info['objid'],info['objid'])
+	info['technician'] = click.prompt(click.style("Please enter your own name",fg='green'),default="Piaras Hoban")
 	info['mnt_point'] = get_dvd_mount_point()
-	return info
+	click.echo(click.style('Information gathered:',fg='blue'))
+	print json.dumps(info,indent=True)
 
-info = inquisition()
+@click.command()
+@click.option('--writedir',envvar='WRITEDIR')
+def run(writedir):
+	inquisition(writedir)
+	create_structure()
+	create_iso()
 
+if __name__ == '__main__':
+	run()
